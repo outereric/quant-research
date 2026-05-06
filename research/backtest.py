4d55a801d61e244bafdceb74bce071515e3daf10
outereric <ericperng0324@gmail.com>
2026-05-06 16:39:49 +0800

    fix backtest to handle series and dataframe

--- research/backtest.py
+++ research/backtest.py
@@ -12,9 +12,19 @@ class Backtest:
     def run(self):
         positions = self.strategy.run(self.prices)
         returns = to_returns(self.prices)
-        pnl = (positions.shift(1) * returns).sum(axis=1)
-        costs = positions.diff().abs().sum(axis=1) * self.transaction_cost
-        gross_exposure = positions.shift(1).abs().sum(axis=1) 
+        pnl = (positions.shift(1) * returns)
+        if hasattr(pnl, 'columns'):
+            pnl = pnl.sum(axis=1)
+        
+        costs = positions.diff().abs()
+        if hasattr(costs, 'columns'):
+            costs = costs.sum(axis=1)
+        costs = costs * self.transaction_cost
+
+        gross_exposure = positions.shift(1).abs()
+        if hasattr(gross_exposure, 'columns'):
+            gross_exposure = gross_exposure.sum(axis=1)
+        
         ret = (pnl - costs) / gross_exposure
         ret = ret.replace([np.inf, -np.inf], np.nan).fillna(0)
         self.results = PerformanceStats(ret)
